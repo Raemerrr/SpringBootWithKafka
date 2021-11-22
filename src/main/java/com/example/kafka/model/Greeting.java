da34354716d9fa199e85616c1418ae8133ef6207
Raemin KANG <foals18@gmail.com>
2021-11-22 21:57:24 +0900

    feat: Kafka Client 추가 업로드
    * Consumer, Producer Config 수정

---
+++ src/main/java/com/example/kafka/model/Greeting.java
@@ -0,0 +1,20 @@
+/*
+ * Greeting
+ * kafka
+ *
+ * Version 0.1
+ *
+ * Created by 강래민 on 2021-11-22.
+ *
+ * Copyright © 2021 raemerrr. All rights reserved.
+ */
+package com.example.kafka.model;
+
+import lombok.Data;
+
+@Data
+public class Greeting {
+
+    private String msg;
+    private String name;
+}
